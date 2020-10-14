b87771e8d0f18a5055259dd3ea260a69341e8211
Shimushushushu <wangsijie@outlook.com>
2020-10-14 23:10:25 +0800

    add rref() and plu()

---
+++ linear-algebra/plu.jl
@@ -0,0 +1,76 @@
+function identical_matrix(n::Integer)
+    ret = zeros(n, n)
+    for itr1 in 1 : n
+        ret[itr1, itr1] = 1
+    end
+    return ret
+end
+
+function exchange_row!(mat::Matrix, a::Integer, b::Integer)
+    # if $a$ or $b$ are out of bound, do nothing
+    if a > size(mat, 1) || b > size(mat, 1)
+        return mat
+    end
+    # exchange row $a$ and row $b$
+    for itr1 in 1 : size(mat, 2)
+        mat[a, itr1], mat[b, itr1] = mat[b, itr1], mat[a, itr1]
+    end
+    return mat
+end
+
+function similarity_transformation!(
+        mat::Matrix, a::Integer, b::Integer)
+    # if $a$ or $b$ are out of bound, do nothing
+    if a > size(mat, 1) || b > size(mat, 1)
+        return mat
+    end
+    # taking advantage of the matrix, the transformation
+    # is just exchanging part of the row $a$ and row $b$
+    for itr1 in 1 : (min(a, b) - 1)
+        mat[a, itr1], mat[b, itr1] = mat[b, itr1], mat[a, itr1]
+    end
+    return mat
+end
+
+function plu(mat::Matrix)
+    # make a copy of the original matrix, which will finally become $U$
+    U = float(copy(mat))
+    # # U = copy(mat)
+    # it's assumed that the matrix has $m$ rows and $n$ columns
+    m, n = size(U)
+    # initialize two identical matrices as $P$ and $L$
+    P = identical_matrix(m)
+    L = identical_matrix(m)
+    # $i$ and $j$ are used to represent iterators of row and column,
+    # respectively
+    i = j = 1
+    while i <= m && j <= n
+        # find the entry with largest absolute value
+        imax, vmax = 0.0, 0.0
+        for itr1 in i : m
+            if vmax < abs(U[itr1, j])
+                imax, vmax = itr1, abs(U[itr1, j])
+            end
+        end
+        (vmax > 0) ? (k = imax) : (j += 1; continue)
+        # left multiply $U$ and $P$ with a permutation matrix
+        if k != i
+            exchange_row!(U, i, k)
+            exchange_row!(P, i, k)
+        end
+        # left and right multiply $L$ with the same matrix
+        similarity_transformation!(L, i, k)
+        # `forward elimination'
+        for itr1 in (i + 1) : n
+            scaler = U[itr1, j] / U[i, j]
+            for itr2 in j : n
+                U[itr1, itr2] -= U[i, itr2] * scaler
+            end
+            # right multiply $L$ with an elimination matrix
+            L[itr1, i] = scaler
+        end
+        i += 1
+        j += 1
+    end
+    return P, L, U
+end
---
+++ linear-algebra/rref.jl
@@ -0,0 +1,67 @@
+function rref(mat::Matrix)
+    # make a copy of the original matrix
+    ret = float(copy(mat))
+    # # ret = copy(mat)
+    # initialize an array to memorize the pivots
+    pivot = []
+    # it's assumed that the matrix has $m$ rows and $n$ columns
+    m, n = size(ret)
+    # $i$ and $j$ are used to represent iterators of row and column,
+    # respectively
+    i = j = 1
+    # part of `forward elimination'
+    while i <= m && j <= n
+        # find the entry with largest absolute value
+        imax, vmax = 0.0, 0.0
+        for itr1 in i : m
+            if vmax < abs(ret[itr1, j])
+                imax, vmax = itr1, abs(ret[itr1, j])
+            end
+        end
+        # but this one seems to work pretty fine?
+        (vmax > 0) ? (k = imax) : (j += 1; continue)
+        # # if vmax > 0
+        # #     k = imax
+        # # else 
+        # #     j += 1
+        # #     continue
+        # # end
+        # add the pivot position to the array
+        push!(pivot, (i, j))
+        println(pivot)
+        # exchange the rows
+        for itr1 in i : n
+            ret[i, itr1], ret[k, itr1] = ret[k, itr1], ret[i, itr1]
+        end
+        # `forward elimination'
+        for itr1 in (i + 1) : m
+            scaler = ret[itr1, j] / ret[i, j]
+            for itr2 in (j + 1) : n
+                ret[itr1, itr2] -= ret[i, itr2] * scaler
+            end
+            # manually set this entry to 0, in case of using
+            # LU factorization
+            ret[itr1, j] = 0
+        end
+        i += 1
+        j += 1
+    end
+    # part of `back subsitution'
+    for itr1 in length(pivot) : -1 : 1
+        # get the pivot
+        i, j = pivot[itr1]
+        # `back subsitution`
+        for itr2 in 1 : (i - 1)
+            scaler = ret[itr2, j] / ret[i, j]
+            for itr3 in j : n
+                ret[itr2, itr3] -= ret[i, itr3] * scaler
+            end
+        end
+        # make entry on the pivot be 1
+        # and a little trick in order not to create a new variable
+        for itr2 in n : -1 : j
+            ret[i, itr2] /= ret[i, j]
+        end
+    end
+    return ret
+end
